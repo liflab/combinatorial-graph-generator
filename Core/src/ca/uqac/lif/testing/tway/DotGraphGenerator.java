1e8fbf0c712d4141766101d3880a6ccb86c63fc0
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2018-02-01 11:05:57 -0500

    Version used for tests (01 fev. 2018)
    Add -n & -v parameter to generate graph and hypergraph without input file
    Add generator for nway variables file (with the new parameters)
    
    Fix graph syntax (add space around --)

--- Core/src/ca/uqac/lif/testing/tway/DotGraphGenerator.java
+++ Core/src/ca/uqac/lif/testing/tway/DotGraphGenerator.java
@@ -16,10 +16,10 @@ public class DotGraphGenerator extends GraphGenerator
 		super.generateTWayEdges();
 		m_output.println("}");
 	}
-	
+
 	@Override
 	public void vertexCallback(long src_id, List<String> tuple1, int tuple_nb1, List<String> values1, long dst_id, List<String> tuple2, int tuple_nb2, List<String> values2)
 	{
-		m_output.println(src_id + "--" + dst_id + ";");
+		m_output.println(src_id + " -- " + dst_id + ";");
 	}
 }
--- Core/src/ca/uqac/lif/testing/tway/FrontEnd.java
+++ Core/src/ca/uqac/lif/testing/tway/FrontEnd.java
@@ -22,31 +22,56 @@ public class FrontEnd
 	public static void main(String[] args)
 	{
 		CliParser parser = new CliParser();
-		parser.addArgument(new Argument().withShortName("t").withArgument("x").withDescription("Generates hyperedges for t=x"));
-		parser.addArgument(new Argument().withLongName("method").withArgument("m").withDescription("Generates graph for method m (coloring, edn, hypergraph)"));
+		parser.addArgument(new Argument().withShortName("t").withArgument("x").withDescription("Generates for t=x"));
+		parser.addArgument(new Argument().withShortName("n").withArgument("x").withDescription("If no input_file : Generates for n=x"));
+		parser.addArgument(new Argument().withShortName("v").withArgument("x").withDescription("If no input_file : Generates for v=x"));
+		parser.addArgument(new Argument().withLongName("method").withArgument("m").withDescription("Generates graph for method m (variables (if -n and -v), coloring, edn, hypergraph)"));
 		parser.addArgument(new Argument().withShortName("o").withArgument("f").withDescription("Output graph to file f"));
 		parser.addArgument(new Argument().withLongName("help").withDescription("Show command line usage"));
 		ArgumentMap arguments = parser.parse(args);
+
 		if (arguments.hasOption("help"))
 		{
-			parser.printHelp("Hypergraph generator", System.out);
+			parser.printHelp("Graph / Hypergraph generator\njava -jar <jar_name> [OPTIONS] input_file\n\nOPTIONS:", System.out);
 			System.exit(0);
 		}
-		if (!arguments.hasOption("t"))
-		{
-			System.err.println("Missing parameter t");
-			System.exit(2);
-		}
+
 		if (!arguments.hasOption("method"))
 		{
 			System.err.println("Missing parameter method");
 			System.exit(2);
 		}
-		int t = Integer.parseInt(arguments.getOptionValue("t"));
 		String method = arguments.getOptionValue("method");
-		String filename = arguments.getOthers().get(0);
+
+		if (!arguments.hasOption("t") && !method.equals("variables"))
+		{
+			System.err.println("Missing parameter t");
+			System.exit(2);
+		}
+
+		PrintStream out = System.out;
+		if (arguments.hasOption("o"))
+		{
+			String out_filename = arguments.getOptionValue("o");
+			try
+			{
+				out = new PrintStream(new FileOutputStream(new File(out_filename)));
+			}
+			catch (FileNotFoundException e)
+			{
+				System.err.println("File not open: " + out_filename);
+				System.exit(2);
+			}
+		}
+
+		String filename = "";
+		List<String> others = arguments.getOthers();
+		if(others.size() > 0)
+			filename = others.get(0);
+
 		TWayGraphProblem twp = null;
 		Map<String,List<String>> domains = null;
+
 		try
 		{
 			domains = parseDomains(new File(filename));
@@ -54,11 +79,37 @@ public class FrontEnd
 		}
 		catch (FileNotFoundException e)
 		{
-			System.err.println("File " + filename + " not found");
-			System.exit(1);
+			if (arguments.hasOption("n") && arguments.hasOption("v")) {
+				int n = Integer.parseInt(arguments.getOptionValue("n"));
+				int v = Integer.parseInt(arguments.getOptionValue("v"));
+				domains = createDomains(n,v);
+			}
+			else
+			{
+				System.err.println("File " + filename + " not found");
+				System.exit(1);
+			}
+
 		}
+
+		if (method.equalsIgnoreCase("variables")) {
+			for(Map.Entry<String,List<String>> var : domains.entrySet()) {
+				out.print(var.getKey() + ':');
+				List<String> vals = var.getValue();
+				for (int i = 0; i < vals.size(); i++) {
+					out.print(vals.get(i));
+					if (i != vals.size()-1)
+						out.print(',');
+				}
+				out.print('\n');
+			}
+			System.exit(0);
+		}
+
 		List<String> var_names = new ArrayList<String>(domains.size());
 		var_names.addAll(domains.keySet());
+		int t = Integer.parseInt(arguments.getOptionValue("t"));
+
 		if (method.equalsIgnoreCase("coloring"))
 		{
 			twp = new DotGraphGenerator(t, var_names);
@@ -76,31 +127,19 @@ public class FrontEnd
 			System.err.println("Unknown method " + method);
 			System.exit(2);
 		}
-		PrintStream out = null;
-		if (arguments.hasOption("o"))
-		{
-			String out_filename = arguments.getOptionValue("o");
-			try 
-			{
-				out = new PrintStream(new FileOutputStream(new File(out_filename)));
-				twp.setOutput(out);
-			} 
-			catch (FileNotFoundException e)
-			{
-				System.err.println("File not found: " + out_filename);
-				System.exit(2);
-			}
-		}
+
+		twp.setOutput(out);
 		twp.addDomain(domains);
 		twp.generateTWayEdges();
+
 		if (out != null)
 			out.close();
 	}
-	
+
 	public static TWayGraphProblem getGenerator(int t, String method, String in_filename, PrintStream out) throws IOException
 	{
 		TWayGraphProblem twp = null;
-		Map<String,List<String>> domains = null;
+		Map<String,List<String>> domains;
 		domains = parseDomains(new File(in_filename));
 		List<String> var_names = new ArrayList<String>(domains.size());
 		var_names.addAll(domains.keySet());
@@ -123,11 +162,22 @@ public class FrontEnd
 		twp.addDomain(domains);
 		return twp;
 	}
+
+	protected static Map<String,List<String>> createDomains(int n, int v) {
+		Map<String,List<String>> domains = new HashMap<String,List<String>>();
+		for (Integer n_iter = 0; n_iter < n; n_iter++) {
+			List<String> values = new ArrayList<String>(v);
+			for (Integer v_iter = 0; v_iter < v; v_iter++) {
+				values.add(v_iter.toString());
+			}
+			domains.put("var" + n_iter.toString(), values);
+		}
+		return domains;
+	}
 	
 	protected static Map<String,List<String>> parseDomains(File f) throws FileNotFoundException
 	{
 		Scanner scanner = new Scanner(f);
-		List<String> var_names = new ArrayList<String>();
 		Map<String,List<String>> domains = new HashMap<String,List<String>>();
 		while (scanner.hasNextLine())
 		{
@@ -136,7 +186,6 @@ public class FrontEnd
 				continue;
 			String[] parts = line.split(":");
 			String var_name = parts[0].trim();
-			var_names.add(var_name);
 			String[] vals = parts[1].split(",");
 			List<String> values = new ArrayList<String>(vals.length);
 			for (String v : vals)
